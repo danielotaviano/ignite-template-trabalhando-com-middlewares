ee919a416ddbda17ae0fd2943f1a71bdc2cc14f2
Daniel Otaviano <danielpontes0@icloud.com>
2021-03-08 22:29:21 -0300

    feat: challenger done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,60 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const existingUser = users.find(user => user.username === username)
+
+  if(!existingUser) {
+    return response.status(404).json({error:"user does not exist"})
+  }
+
+  request.user = existingUser
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+    const { user } = request
+
+    if(user.todos.length < 10 || user.pro) {
+      return next()
+    }
+
+    return response.status(403).json({error:'user doest not have permission to create todo'})
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+  if(!user ) return response.status(404).json({error: 'user does not exist'})
+
+  if(!validate(id)) return response.status(400).json({error:'id must be a uuid'})
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) return response.status(404).json({error:'todo doest not exist'})
+
+
+  request.todo = todo
+  request.user = user
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) return response.status(404).json({error: 'user not found'})
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
